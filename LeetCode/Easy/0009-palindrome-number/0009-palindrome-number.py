0a94ff537d1592a80f45c435fc15d39c23aa9369
syoo0295 <72421499+syoo0295@users.noreply.github.com>
2023-06-28 23:20:26 -0700

    Time: 74 ms (65.35%), Space: 16.2 MB (77.24%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # base case: if x is negative or if the last digit is 0, it returns false
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
