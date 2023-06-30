df8a683e295aab026b472e0ab902396aede72439
syoo0295 <72421499+syoo0295@users.noreply.github.com>
2023-06-29 21:09:32 -0700

    Time: 44 ms (77.31%), Space: 16.4 MB (51.46%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Initialize an empty stack 
-        stack = []
-        brackets_map = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
-        
-        for char in s:
-            # if char is an open bracket, push it on the the stack
-            if char in ['(', '{', '[']:
-                stack.append(char)
-            elif char in [')', '}', ']']:
-                # if stack is empty or top of the stack is not the corresponding opening bracket, return False.
-                if not stack or stack[-1] != brackets_map[char]:
-                    return False
-                stack.pop()
-        return len(stack) == 0
+       cto_o={')':'(',']':'[', '}':'{'}
+       stack=[]
+       for i in s:
+           if i in cto_o:
+              if stack and stack[-1] == cto_o[i]:
+                   stack.pop()
+              else:
+                  return False
+           else :
+                stack.append(i)
+       return False if stack else True
